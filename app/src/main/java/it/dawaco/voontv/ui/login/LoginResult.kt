362adb799db54ac0dab75fd6dd7a82a663615d3e
voontv <voontv@gmail.com>
2020-08-17 01:17:13 +0700

    created login Layout

---
+++ app/src/main/java/it/dawaco/voontv/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package it.dawaco.voontv.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
